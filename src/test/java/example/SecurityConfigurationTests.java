c4cb9d87e51e4adaecbb6e96900a15ed9ea3a96e
Michael Perdue <michael0perdue@gmail.com>
2024-08-03 03:36:20 +0100

    Added unit test for login route

---
+++ src/test/java/example/SecurityConfigurationTests.java
@@ -0,0 +1,35 @@
+package example;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SecurityConfigurationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void validLogin() throws Exception {
+        mockMvc.perform(post("/login")
+                        .param("username", "username")
+                        .param("password", "password"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/file/names"));
+    }
+
+}
